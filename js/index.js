a0d70ddf3aed7cb67356a571c6246dfc7c5d18f0
mshehadeh <mshe246@gmail.com>
2022-04-26 02:09:44 +1200

    clean up my website

--- js/index.js
+++ js/index.js
@@ -1,98 +1,83 @@
-$(document).ready(function() {
+// grabbing references to spans that will be changing text
+const front = document.getElementById('front');
+const back = document.getElementById('back');
+const multiple = 100;
 
-  // grabbing references to spans that will be changing text
-  var front = $("#front-end");
-  var back = $("#back-end");
-  var name = $("#header");
+function selectFront() {
+  front.classList.add('selected');
+}
 
-  // get the magic started
-  setTimeout(selectFront, 2000);
-  setTimeout(deleteFront, 3000);
-  setTimeout(startTyping, 4000);
+function deleteFront() {
+  front.textContent = '';
+}
 
-  function selectFront() {
-    front.addClass("selected");
-  }
+function addText(newText) {
+  back.textContent = newText;
+}
 
-  function deleteFront() {
-    front.text("")
-  }
+// could combine these methods with
+// their front counter parts by
+// just passing a reference to the element
+// but i think keeping them seperate
+// makes it easier to follow.
+function selectBack() {
+  back.classList.add('selected');
+}
 
-  function startTyping() {
-    var stringsToType = [
-      "is a software engineer.",
-      "makes websites.",
-      "can school you in fifa. \uD83D\uDD25",
-      "enjoys solving problems.",
-      "writes code. \uD83D\uDC4C"
-    ]
-
-    var time = 0;
-    var timeForPreviousWord = 0;
-    var timeForCurrentWord = 0;
-    var multiple = 100;
-
-    for (var i = 0; i < stringsToType.length; i++) {
-      timeForCurrentWord = (multiple * stringsToType[i].length) * 2 + 1000;
-      if (i == stringsToType.length - 1) {
-        setTimeout(typeText, timeForPreviousWord + 2000, stringsToType[i]);
-      } else {
-        setTimeout(typeTextAndDelete, timeForPreviousWord + 2000, stringsToType[i]);
-      }
-
-      timeForPreviousWord = timeForCurrentWord + timeForPreviousWord;
-    }
-  }
-
-
-function typeTextAndDelete(goalString) {
-  var currentString = "";
-  var multiple = 100;
-
-  for (var i = 0; i < goalString.length; i++) {
-    currentString = currentString + goalString[i];
-    if (goalString.charCodeAt(i) > 255) {
-      currentString = currentString + goalString[i+1];
-      i =  i + 1;
-    }
-    setTimeout(addText, multiple * i, currentString);
-  }
-
-  var time = (goalString.length * multiple) * 2 ;
-  setTimeout(selectBack, time);
-  setTimeout(deleteBack, time + 500 );
+function deleteBack() {
+  back.textContent = '';
+  back.classList.remove('selected');
 }
 
 function typeText(goalString) {
-  var currentString = "";
-  var multiple = 100;
+  let currentString = '';
 
-  for (var i = 0; i < goalString.length; i++) {
-    currentString = currentString + goalString[i];
+  for (let i = 0; i < goalString.length; i += 1) {
+    currentString += goalString[i];
     if (goalString.charCodeAt(i) > 255) {
-      currentString = currentString + goalString[i+1];
-      i =  i + 1;
+      currentString += goalString[i + 1];
+      i += 1;
     }
     setTimeout(addText, multiple * i, currentString);
   }
 }
 
+function typeTextAndDelete(goalString) {
+  typeText(goalString);
 
-  function addText(newText) {
-    back.text(newText);
-  }
+  const time = (goalString.length * multiple) * 2;
+  setTimeout(selectBack, time);
+  setTimeout(deleteBack, time + 500);
+}
 
-  // could combine these methods with
-  // their front counter parts by
-  // just passing a reference to the element
-  // but i think keeping them seperate
-  // makes it easier to follow.
-  function selectBack() {
-    back.addClass("selected");
-  }
+function startTyping() {
+  const stringsToType = [
+    'is a software engineer.',
+    'makes websites.',
+    'can school you in fifa. \uD83D\uDD25',
+    'enjoys solving problems.',
+    'writes code. \uD83D\uDC4C',
+  ];
 
-  function deleteBack() {
-    back.text("")
-    back.removeClass("selected");
-  }
+  let timeForPreviousWord = 0;
+  let timeForCurrentWord = 0;
+
+  stringsToType.forEach((string, index) => {
+    timeForCurrentWord = (multiple * string.length) * 2 + 1000;
+    const func = index === (stringsToType.length - 1) ? typeText : typeTextAndDelete;
+    setTimeout(func, timeForPreviousWord + 2000, string);
+    timeForPreviousWord = timeForCurrentWord + timeForPreviousWord;
+  });
+}
+
+setTimeout(selectFront, 2000);
+setTimeout(deleteFront, 3000);
+setTimeout(startTyping, 4000);
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    card.firstElementChild.classList.toggle('flipped');
+  });
 });
